221eddc556479afb2b58f0c996650ef131d6d696
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2026-08-10 09:36:55 +0100

    feat: implement comment moderation and update event handling Add a docker file

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,20 +5,28 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', async (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     try {
             await axios.post('http://localhost:4000/events', event);
             await axios.post('http://localhost:4001/events', event);
             await axios.post('http://localhost:4002/events', event);
-
+            await axios.post('http://localhost:4003/events', event);
     } catch (error) {
         console.error('Error occurred while posting event:', error);
     }
 
     res.send({ status: 'OK' });
 });
+
+app.get('/events', (req, res) => {
+    res.send(events)
+})
 
 
 app.listen(4005, () => {
