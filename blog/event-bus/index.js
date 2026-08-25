f3a99b1b2bddeb9cc70b8157c58a4beb406ee2ea
meedaah19 <hameedattimileyinoyewopo@gmail.com>
2026-08-25 16:07:58 +0100

    feat: update event bus communication and Kubernetes configurations

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -13,10 +13,10 @@ app.post('/events', async (req, res) => {
     events.push(event);
 
     try {
-            await axios.post('http://localhost:4000/events', event);
-            await axios.post('http://localhost:4001/events', event);
-            await axios.post('http://localhost:4002/events', event);
-            await axios.post('http://localhost:4003/events', event);
+            await axios.post('http://posts-clusterip-srv:4000/events', event);
+            // await axios.post('http://localhost:4001/events', event);
+            // await axios.post('http://localhost:4002/events', event);
+            // await axios.post('http://localhost:4003/events', event);
     } catch (error) {
         console.error('Error occurred while posting event:', error);
     }
